b5400765dbe73c84e17f142d6cbcf430a407eca3
priyanka66 <priyanka@hashedin.com>
2014-06-27 16:47:19 +0530

    Done with View 2 Layout

--- DOIT/WebContent/app/js/controllers.js
+++ DOIT/WebContent/app/js/controllers.js
@@ -27,13 +27,34 @@ angular.module('myApp.controllers', [])
                      }
                  ];
     
-  }]);
+  }])
   .controller('ProjectList', ['$scope','$routeParams', function($scope,$routeParams) {
     $scope.message = "Hello This message is from View 2";
        $scope.projects1= [
                      {   "name"  : "Hu-Assignment1",
-                         "description": " First assignment for demo purpose",
-                         "coordinators": ["Rahul","Nagarjun"]
+                    	 "coordinators": ["Rahul","Nagarjun"],
+                    	 "description": " First assignment for demo purpose",
+                    	 "tasks":[
+								{
+									"taskName" : "Create index page",
+									"taskDescription" : "Create a home page for the project",
+									"priority" : "0",
+									"assignedTo" : "Priyanka",
+									"dueDate" : 	"29/06/2014",
+									"createdOn" : "27/06/2014"
+								},
+
+								{
+									"taskName" : "Improve UI & UX",
+									"taskDescription" : "Create more pleasing UI",
+									"priority" : "2",
+									"assignedTo" : "Priyanka",
+									"dueDate" : "01/07/2014",
+									"createdOn" : "27/06/2014"
+								}
+							]
+                    	 
+                         
                      },
                      {
                     	 "name"  : "Hu-Assignment2",
@@ -52,13 +73,15 @@ angular.module('myApp.controllers', [])
                      }
                  ];
        
-       console.log($scope.projects1);
-		for (var i=0;i<$scope.project1.length;i++)
+      
+		for (var i=0;i<$scope.projects1.length;i++)
 			{
 				if($scope.projects1[i].name==$routeParams.name)
 					{
 						$scope.details=$scope.projects1[i];
+						
 					}
+				
 			}
   }]);
 
